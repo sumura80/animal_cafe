e77596fd08a76efff199e33ce5076bb0a3d0090c
Susumu <sumura80@gmail.com>
2018-09-06 21:18:00 +0900

    changed title name

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,18 @@
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       # Amazon S3用の設定
+#       :provider              => 'AWS',
+#       :region                => ENV['AMAZON_S3_REGION'], 
+#       :aws_access_key_id     => ENV['AMAZON_ACCESS_KEY_ID'],
+#       :aws_secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+#     }
+#     config.fog_directory     =  ENV['AMAZON_S3_BUCKET_NAME']
+#   end
+# end
+
+
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
@@ -10,4 +25,3 @@ if Rails.env.production?
     config.fog_directory     =  ENV['AMAZON_S3_BUCKET_NAME']
   end
 end
-
